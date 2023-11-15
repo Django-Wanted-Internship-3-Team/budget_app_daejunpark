678224acd51d598cb15af8b4e80f1931f8560342
chestnut90 <cowzon90@gmail.com>
2023-11-15 16:03:53 +0900

    :recycle:Refact : 사용자 로그인 API를 simplejwt TokenObtainPairView 로 변경 #3

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.views import SignInAPIView, SignUpAPIView
+from users.views import SignUpAPIView
 
 urlpatterns = [
     path("signup/", view=SignUpAPIView.as_view(), name="signup"),
-    path("signin/", view=SignInAPIView.as_view(), name="signin"),
+    path("signin/", view=TokenObtainPairView.as_view(), name="signin"),
 ]
