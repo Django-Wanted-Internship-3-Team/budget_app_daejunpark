63a7d6873f8192c1587437a69c0f35dce77834d6
chestnut90 <cowzon90@gmail.com>
2023-11-15 09:24:50 +0900

    :sparkles:Feat : 사용자 회원가입 API View 추가 #2
    Related to #2

--- src/users/views.py
+++ src/users/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class SignUpAPIView(APIView):
+    @swagger_auto_schema(
+        operation_summary="사용자 회원가입",
+    )
+    def post(self, request):
+        return Response()
