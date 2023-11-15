373d97471dacbfbd25e0ef6222d7f12ff1e520ec
chestnut90 <cowzon90@gmail.com>
2023-11-15 09:34:07 +0900

    :test_tube:Failing Test : 회원가입 테스트 케이스 #2
    - 정상 테스트 및 중복, 패스워드 체크 테스트
    Related to #2

--- src/users/tests/test_signup_view.py
+++ src/users/tests/test_signup_view.py
@@ -1,10 +1,52 @@
+from django.contrib.auth import get_user_model
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 
 class SingUpAPITestCase(APITestCase):
     viewname = "signup"
 
-    def test_no_view_failure(self):
-        """지정된 view가 없는 실패 테스트 케이스"""
-        self.client.post(path=reverse(self.viewname))
+    def test_signup_success(self):
+        """사용자 회원가입 성공 테스트 케이스"""
+
+        username = "user"
+        password = "asdfn@1#2"
+
+        response = self.client.post(
+            path=reverse(self.viewname),
+            data={"username": username, "password": password},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # response no show user password
+        self.assertIsNone(response.data.get("password", None))
+
+        # password check
+        user = get_user_model().objects.get(username=username)
+        self.assertTrue(user.check_password(password))
+
+    def test_signup_simple_password_error(self):
+        """사용자 회원가입시 간단한 비밀번호 방지 테스트 케이스"""
+        response = self.client.post(
+            path=reverse(self.viewname),
+            data={"username": "user", "password": "12345678"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_signup_duplicated_username_error(self):
+        """사용자 회원가입시 중복 사용자이름 방지 테스트 케이스"""
+
+        username = "user"
+
+        # create user
+        get_user_model().objects.create(username=username)
+
+        response = self.client.post(
+            path=reverse(self.viewname),
+            data={"username": username, "password": "asdfjwn!32S@"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
